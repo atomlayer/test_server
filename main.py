5d8089da0d9ecc7d2bee57287d22616e85bfc783
aa.lavrentev <aa.lavrentev@tensor.ru>
2023-02-02 22:00:19 +0300

    add serve

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 app.register_blueprint(auth)
 app.register_blueprint(player)
 
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    from waitress import serve
+
+    # app.run(host='0.0.0.0', debug=True)
+    serve(app, host='0.0.0.0', port=5000)
